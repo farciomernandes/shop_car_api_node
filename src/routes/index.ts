4021534047108d4bc33137e66f73fc8c779f9258
Marcio Fernandes <farciomernandes@gmail.com>
2022-05-22 14:50:35 -0300

    adiciona rota de login com criacao de token 1 dia

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { categoriesRoutes } from './categories.routes';
 import { specificationRoutes } from './specification.routes';
 import { usersRoutes } from './users.routes';
+import { authenticateRoutes } from './authenticate.routes';
 
 
 const router = Router();
@@ -10,6 +11,9 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoutes);
 router.use("/users", usersRoutes);
+
+router.use(authenticateRoutes);
+
 
 
 
