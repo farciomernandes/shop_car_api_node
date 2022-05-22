d720e77963f1c4a17d97696d9c8fef6b58de168e
Marcio Fernandes <farciomernandes@gmail.com>
2022-05-22 09:37:36 -0300

    cria entidade users e cria migration que retira coluna username de users.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,12 @@
 import { container } from "tsyringe";
-import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 
 
 
@@ -14,4 +18,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
-)
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository    
+);
